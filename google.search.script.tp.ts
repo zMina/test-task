dbe1d3f240d80ff8ad95f0d06e7d4aef2535e316
Zina <zinaida.mazurkevych@gmail.com>
2019-03-28 11:02:35 +0200

    Update google.search.script.tp.ts

--- google.search.script.tp.ts
+++ google.search.script.tp.ts
@@ -1,14 +1,21 @@
 import {browser, by, element} from 'protractor';
 
-const SEARCH_FIELD_LOCATOR = '//input[@class="gLFyf gsfi"]' ;
-const SUBMIT_BUTTON_LOCATOR = '//div[@class="FPdoLc VlcLAe"]//input[@name="btnK"]';
-const SEARCH_STRING = 'some text which is better to keep in separate file:)';
+const SEARCH_FIELD_LOCATOR = '//input[@name="q"]';
+const LISTBOX_LOCATOR = '//li[@id="sbt"]/parent::div';
+const SEARCH_STRING = 'some text';
+const LISTBOX_SUBMIT_BUTTON_LOCATOR = '//li[@id="sbt"]/parent::div//input[@name="btnK"]';
+const SUBMIT_BUTTON_LOCATOR = '//div[@jsname="UUbT9"]/following-sibling::div//input[@name="btnK"]';
 
-describe('Google search script', function() {
-  it('Get search field and enter text', function() {
+describe('Google search script', () => {
+  it('Get search field and enter text', async () => {
     browser.get('https://google.com');
 
     element(by.xpath(SEARCH_FIELD_LOCATOR)).sendKeys(SEARCH_STRING);
-    element(by.xpath(SUBMIT_BUTTON_LOCATOR)).click();
+    // To handle case when no proposed search string appears
+    if (await (element(by.xpath(LISTBOX_LOCATOR))).isDisplayed()) {
+      element(by.xpath(LISTBOX_SUBMIT_BUTTON_LOCATOR)).click();
+    } else {
+      element(by.xpath(SUBMIT_BUTTON_LOCATOR)).click();
+    }
   });
 });
